da6d1c1cd8e6b818ccc8c819ffe9bbc79d74bf95
zephyloth <zephyloth@msn.com>
2023-10-13 23:11:40 +0300

    Güncel
    Güncel

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform target; // Reference to the object you want the camera to follow
+    public float smoothSpeed = 0.125f; // Speed at which the camera follows the target
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (target != null)
+        {
+            // Calculate the target position with the same y and z values as the camera
+            Vector3 targetPosition = new Vector3(target.position.x, transform.position.y, transform.position.z);
+
+            // Use Vector3.Lerp to smoothly move the camera towards the target position
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, smoothSpeed);
+
+            // Set the camera's position to the smoothed position
+            transform.position = smoothedPosition;
+        }
     }
 }
--- Assets/Scripts/CharController.cs
+++ Assets/Scripts/CharController.cs
@@ -6,16 +6,50 @@ using UnityEngine;
 public class CharController : MonoBehaviour
 {
     Rigidbody Rigidbody;
+    SphereCollider SphereCollider;
+
     public float MovementSpeed;
     public float Gravity;
     public float JumpFactor;
+    public float GroundCheckDistance;
     float CurrentSpeed;
-    
+    public bool Grounded;
     Vector3 Direction;
 
+    int PlayerLayer;
     void Start()
     {
-        Rigidbody = GetComponent<Rigidbody>();  
+        Rigidbody = GetComponent<Rigidbody>();
+        SphereCollider = GetComponent<SphereCollider>();
+
+        PlayerLayer = LayerMask.NameToLayer("Player");
+    }
+
+    bool IsGrounded()
+    {
+        RaycastHit hit;
+        var Pos = transform.position;
+ 
+        Vector3 rayStart = Pos; // Adjust the offset to avoid self-collision
+        Vector3 rayDirection = Vector3.down;
+
+   
+        if (Physics.Raycast(rayStart, rayDirection, out hit, float.MaxValue, ~PlayerLayer))
+        {
+            Debug.Log(hit.distance - SphereCollider.radius);
+            if (hit.distance - SphereCollider.radius < GroundCheckDistance)
+                return true;
+
+        }
+        return false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        return;
+        var Pos = transform.position;
+        Pos.y -= SphereCollider.radius;
+        Gizmos.DrawCube(Pos, Vector3.one); 
     }
 
     // Update is called once per frame
@@ -23,17 +57,24 @@ public class CharController : MonoBehaviour
     {
         float Horizontal = Input.GetAxis("Horizontal");
         CurrentSpeed = Horizontal * MovementSpeed;
- 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Rigidbody.AddForce(Vector3.up * JumpFactor, ForceMode.Impulse);
-        }
 
         Vector3 Vel = Rigidbody.velocity;
         Vel.x = CurrentSpeed;
+  
+
+        Grounded = IsGrounded();
+        if (IsGrounded())
+        {
+            Vel.y = 0;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Rigidbody.AddForce(Vector3.up * JumpFactor, ForceMode.Impulse);
+            }
+
+        }
         Rigidbody.velocity = Vel;
 
-        //Rigidbody.AddForce(Vector3.right * CurrentSpeed, ForceMode.Acceleration);
-        Rigidbody.AddForce(Gravity * Vector3.down, ForceMode.Force);
+       // Rigidbody.AddForce(Vector3.right * CurrentSpeed, ForceMode.Acceleration);
+       // Rigidbody.AddForce(Gravity * Vector3.down, ForceMode.Force);
     }
 }
